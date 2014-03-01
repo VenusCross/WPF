3bcdd7c5c5379a611ffa3022bb9564c92d1f8f92
venus <vcrossfullsail@gmail.com>
2014-02-28 20:16:49 -0500

    initial setup

---
+++ js/script.js
@@ -0,0 +1,94 @@
+// JavaScript
+
+
+// practice from worksheet
+var remainder = 50%10;
+console.log(remainder);
+ 
+ 
+ //AVERAGE SHOPPING BILL
+ 
+var groceryList = [70,50,95,87,99]; //amounts spent for five days
+var total = groceryList[0]+[1]+[2]+[3]+[4]/5;// adding all totals then divide by 5 for average amount spent each day of week 
+console.log(total); // total after computation
+ 
+
+
+ // Prompt and Alert
+var userInput = prompt("enter your your year of birth");
+console.log(userInput);
+ 
+ //Another example of Prompt and Alert
+ 	
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
+var height = prompt("Please enter the height");
+ 	
+var area = width * height; //calculates with that info
+ 	//console.log(area);// prints it out to console
+ 	
+alert("The area of the rectangle is " + area + "sq feet");
+alert (result);
+ 
+ //CONDITIONS
+ //BASIC CONDITIONAL STATEMENT
+  
+var oldEnough = true;
+  //if the child is old enough, print to the console "you can ride!"
+  
+if (oldEnough){
+	  //code performed if condition is true
+	  console.log("You can ride the coaster!");
+	  
+}
+  console.log("what comes after");
+  
+  // Conditional Logic - Relational Expressions
+var kidHeight = 40;
+var minHeight = 48;
+  
+  // if the child is old enough, print to the console "you can ride!"
+  // if the kid is over 48 inches in height
+if (kidHeight > minHeight){
+	//code performed if condition is true
+	console.log("You can ride the coaster!");	
+}
+// Conditional Logic - With an Expression 
+
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2; 
+  // if the child is old enough, print to the console "you can ride!"
+  // if the kid is over 48 inches in height
+if (kidHeight + 2 > minHeight){
+	//code performed if condition is true
+	console.log("You can ride the coaster!");	
+}
+
+//if and else
+var kidHeight = 52;
+var minHeight = 48;
+ 
+  // if the child is old enough, print to the console "you can ride!"
+  // if the kid is over 48 inches in height
+if (kidHeight  > minHeight){
+	//code performed if condition is true
+	console.log("You can ride the coaster!");	
+}else{
+	console.log ("Sorry kid, you got some growing to do first");
+} 
+
+// Else if
+var kidHeight = 52;
+var minHeight = 48;
+ 
+  // if the child is old enough, print to the console "you can ride!"
+  // if the kid is over 48 inches in height
+if (kidHeight  > minHeight){
+	//code performed if condition is true
+	console.log("You can ride the coaster!");	
+}else{
+	console.log ("Sorry kid, you got some growing to do first");
+} 
+
+
+   
