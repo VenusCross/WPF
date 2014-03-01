2122a76d7c34aaa018cae7cc901056994a693c90
venus <vcrossfullsail@gmail.com>
2014-03-01 15:34:58 -0500

    practicing on function worksheet for circumference and stung

--- wpf lessons/ASSIGNMENTS/Functions_worksheet/js/functionws.js
+++ wpf lessons/ASSIGNMENTS/Functions_worksheet/js/functionws.js
@@ -1,8 +1,21 @@
-// Calculate the circumference if a circle
+// Calculate the circumference of a circle
 function circumCircle(radius){
 	var radius=5;
 	var circle = 2*3.14*(radius);
 	return circumCircle;
-}
 	console.log("The circumference of the circle is X");
+}	
+circumCircle();	
+	
+	
+	
+// Stung!
 
+var total = beeStings()
+function beeStings(){
+	var victimWeight = 100;
+	var numberOfBeeStings=8.666666667 * victimWeight
+	return numberOfBeeStings;
+	
+}	
+console.log(total);
