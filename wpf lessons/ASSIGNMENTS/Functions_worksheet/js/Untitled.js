24723972805bcb611ace36ed1066407f929f67f5
venus <vcrossfullsail@gmail.com>
2014-03-01 09:10:32 -0500

    working on worksheet formulas function worksheet and reorganize files

---
+++ wpf lessons/ASSIGNMENTS/Functions_worksheet/js/Untitled.js
@@ -0,0 +1,7 @@
+// Calculat the circumference if a circle
+function CircumCircle(radius){
+	var circumference;
+	circumference = 2*3.14*(radius);
+	return circumference;
+	console.log("The circumference of the circle is X");
+}
---
+++ wpf lessons/ASSIGNMENTS/Functions_worksheet/js/functionws.js
@@ -0,0 +1,7 @@
+// Calculat the circumference if a circle
+function CircumCircle(radius){
+	var circumference;
+	circumference = 2*3.14*(5);
+	return circumference;
+	console.log("The circumference of the circle is X");
+}
