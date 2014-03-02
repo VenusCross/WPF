6cd448e39b4f65d8f72d829581777757ff717d10
venus <vcrossfullsail@gmail.com>
2014-03-02 10:42:32 -0500

    added html file for expression industry assignment folder

---
+++ wpf lessons/ASSIGNMENTS/ Expressions_Industry/js/expressindustry.js
@@ -0,0 +1,12 @@
+//Venus Cross Expression Industry Assigment March 2, 2014
+
+ // I work at a hospital people are weighed in kilograms let's convert their weight to pounds
+ 
+ // Problem: Convert 179 kg to pounds
+ 
+ 
+ var oneKg = 2.21 
+ var ptWeightInKg = 179
+ var pounds = oneKg * ptWeightInKg
+ var total = pounds
+ console.log("The patient weighs +"pounds"+"lbs".")
