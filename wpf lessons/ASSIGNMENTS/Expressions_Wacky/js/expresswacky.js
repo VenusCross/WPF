0ffbdba40ec3ff9e6d47ab6afb16d0f0c66eeb58
venus <vcrossfullsail@gmail.com>
2014-03-02 15:38:26 -0500

    plugging in other numbers to see if formula works

--- wpf lessons/ASSIGNMENTS/Expressions_Wacky/js/expresswacky.js
+++ wpf lessons/ASSIGNMENTS/Expressions_Wacky/js/expresswacky.js
@@ -12,3 +12,9 @@ var krispyKremeDonuts = 20 // number of donuts donated to kindergarten class
 var remainder = krispyKremeDonuts % kindergartenStudents // formula to get answer for problem
 console.log(remainder)// this will print the answer to the console for formula given
 // trying the modulo (remainder) operator 
+
+
+var kindergartenStudents = 69 // number of students in class
+var krispyKremeDonuts = 111 // number of donuts donated to kindergarten class
+var remainder = krispyKremeDonuts % kindergartenStudents // formula to get answer for problem
+console.log(remainder)// this will print the answer to the console for formula given
