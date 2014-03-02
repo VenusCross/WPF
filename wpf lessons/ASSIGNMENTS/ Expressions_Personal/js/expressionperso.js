8b02c47a1289fce0a8e7ad7ebf550a27296ef97e
venus <vcrossfullsail@gmail.com>
2014-03-01 21:15:20 -0500

    set variables for problem

--- wpf lessons/ASSIGNMENTS/ Expressions_Personal/js/expressionperso.js
+++ wpf lessons/ASSIGNMENTS/ Expressions_Personal/js/expressionperso.js
@@ -4,6 +4,12 @@ Date()
 
 
 
-//Weightloss Fatburner pills ... How many capsules in a year?
+/*//Weightloss Fatburner pills ... How many capsules in a year where taken 2013? There are 30pills in one bottle. 
+A half of pill is taken daily. There are 365 days in a year.  How many pills have been taken from January 1, 2013 to today date?*/
+
+
+var bottle = 30
+var prescribed = .5
+var daysInYear = 365
 
 
