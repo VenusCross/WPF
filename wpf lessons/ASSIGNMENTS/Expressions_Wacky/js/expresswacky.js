9db1cc79da4b68d3cf5e1de8bc9dd0217b21605d
venus <vcrossfullsail@gmail.com>
2014-03-02 14:51:08 -0500

    changed to correct number of student

--- wpf lessons/ASSIGNMENTS/Expressions_Wacky/js/expresswacky.js
+++ wpf lessons/ASSIGNMENTS/Expressions_Wacky/js/expresswacky.js
@@ -5,6 +5,8 @@
 
 
 // Krispy Kreme Donuts donates donuts to the kindergarten class at Bellview School.
+// How many donuts will each child get.
 
-var kindergartenStudents = 100 // number of students in class
+var kindergartenStudents = 44 // number of students in class
 var krispyKremeDonuts =111 // number of donuts donated to kindergarten class
+var numberOfDonutsToEach = krispyKremeDonuts % kindergartenStudents
