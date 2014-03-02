8de403cba64b953ff5a3e93c39f83435423a17fe
venus <vcrossfullsail@gmail.com>
2014-03-01 22:39:36 -0500

    console was placed

--- wpf lessons/ASSIGNMENTS/ Expressions_Personal/js/expressionperso.js
+++ wpf lessons/ASSIGNMENTS/ Expressions_Personal/js/expressionperso.js
@@ -11,7 +11,7 @@ A half of pill is taken daily. There are 365 days in a year.  How many pills hav
 var bottle = 30 // number of capsules a bottle
 var prescribed = .5 // amount prescribed 
 var daysInYear = 365 // number of days in a year
-var total = prescribed * dayInYear 
+var total = prescribed * dayInYear // calculations for answer 
 var totalHalfmonth = bottle / prescribed
-
-console.log
+var daysInMonth = 30
+console.log(total)
