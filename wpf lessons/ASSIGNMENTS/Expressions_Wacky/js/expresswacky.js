df9cb812e402894ba095dc2eae8b1953e2e37810
venus <vcrossfullsail@gmail.com>
2014-03-02 14:53:48 -0500

    trying to see if I can get decimal

--- wpf lessons/ASSIGNMENTS/Expressions_Wacky/js/expresswacky.js
+++ wpf lessons/ASSIGNMENTS/Expressions_Wacky/js/expresswacky.js
@@ -7,7 +7,7 @@
 // Krispy Kreme Donuts donates donuts to the kindergarten class at Bellview School.
 // How many donuts will each child get.
 
-var kindergartenStudents = 44 // number of students in class
+var kindergartenStudents = 44.00 // number of students in class
 var krispyKremeDonuts =111 // number of donuts donated to kindergarten class
 var numberOfDonutsToEach = krispyKremeDonuts % kindergartenStudents
 console.log(numberOfDonutsToEach)
