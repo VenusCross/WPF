924c466f208932c5749e5fdfce4170d23dda03ed
venus <vcrossfullsail@gmail.com>
2014-03-02 21:56:21 -0500

    initial set up

---
+++ wpf lessons/ASSIGNMENTS/Conditionals_Industry/js/conditionindustry.js
@@ -0,0 +1,6 @@
+// Venus Cross March 2, 2014 Conditional Industry Assignment
+
+//  Criteria for Who Should Get Vaccinated Against Influenza
+
+var fluAge = prompt("Enter your Age","");
+var fluShotAge = true; // boolean true or false
