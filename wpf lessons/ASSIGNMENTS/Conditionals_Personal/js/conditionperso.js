82f831c45914146b56077de9900a28c65dabf0b5
venus <vcrossfullsail@gmail.com>
2014-03-02 20:19:30 -0500

    still making conditions

--- wpf lessons/ASSIGNMENTS/Conditionals_Personal/js/conditionperso.js
+++ wpf lessons/ASSIGNMENTS/Conditionals_Personal/js/conditionperso.js
@@ -6,12 +6,16 @@
 var userIncome = prompt("What is your income?","");// asking user to input income
 var moneyMaker = true; // Boolean - True/False
  
-if (moneyMaker && userIncome >= 75,000){ // used && compound
-	alert("You need to relax and get Bonus check for good work!")
+if (moneyMaker && userIncome >= 75000){ // used && compound
+	alert("You need to relax and get Bonus check for good work!")// want this to pop up on screen
 	
 }else{ 
 
-if (userIncome < 750000)
+if (userIncome < 750000){
+ console.log ("You need to go to school for Training")	
+}else{ if (userIncome < 80000)
+	console.log("Please be grateful for your job")
+}
 
 	
-} 
+
