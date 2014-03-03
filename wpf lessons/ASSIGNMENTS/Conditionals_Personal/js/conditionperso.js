34b4e231a1a52fa7e510e2655ef2d2d5b9b67b5b
venus <vcrossfullsail@gmail.com>
2014-03-02 20:37:17 -0500

    added comment again for last alert to pop

--- wpf lessons/ASSIGNMENTS/Conditionals_Personal/js/conditionperso.js
+++ wpf lessons/ASSIGNMENTS/Conditionals_Personal/js/conditionperso.js
@@ -12,7 +12,7 @@ if (moneyMaker && userIncome >= 75000){ // used && compound
 }else{ if(userIncome < 750000)// states if user income is less than 75000 then my alert should pop up
  	alert("You need to go to school for Training");}// pop up on screen 	
  	{ if(userIncome < 80000)
-	alert("Please be grateful for your job");
+	alert("Please be grateful for your job");// another pop up to screen
 }
 
 	
