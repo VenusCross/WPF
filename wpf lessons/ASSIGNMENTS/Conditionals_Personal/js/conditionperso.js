baada93d9d0d4b4e1db3803907d256bcffb710e7
venus <vcrossfullsail@gmail.com>
2014-03-02 20:50:26 -0500

    moved code up under the greater equality

--- wpf lessons/ASSIGNMENTS/Conditionals_Personal/js/conditionperso.js
+++ wpf lessons/ASSIGNMENTS/Conditionals_Personal/js/conditionperso.js
@@ -8,15 +8,15 @@ var moneyMaker = true; // Boolean - True/False
  
 if (moneyMaker && userIncome >= 75000){ // used && compound
 	alert("You need to relax and get Bonus check for good work!")// want this to pop up on screen
-	
+		{ if(userIncome > 150000);// if greater than this salary annually
+	alert("Ask to be CEO!!!!");
+}
+
 }else{ if(userIncome < 750000)// states if user income is less than 75000 then my alert should pop up
  	alert("You need to go to school for Training");}// pop up on screen 	
  	{ if(userIncome < 80000);
 	alert("Please be grateful for your job");// another pop up to screen
 	console.log("Please be Happy!");}
-	{ if(userIncome > 150000);// if greater than this salary annually
-	alert("Ask to be CEO!!!!");
-}
-
+	
 	
 
