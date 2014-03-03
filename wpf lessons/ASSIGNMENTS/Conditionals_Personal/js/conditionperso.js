9e9aa9d80898743564c7f9be9008c01d6036c5eb
venus <vcrossfullsail@gmail.com>
2014-03-02 20:34:32 -0500

    added comment for ease of code

--- wpf lessons/ASSIGNMENTS/Conditionals_Personal/js/conditionperso.js
+++ wpf lessons/ASSIGNMENTS/Conditionals_Personal/js/conditionperso.js
@@ -9,7 +9,7 @@ var moneyMaker = true; // Boolean - True/False
 if (moneyMaker && userIncome >= 75000){ // used && compound
 	alert("You need to relax and get Bonus check for good work!")// want this to pop up on screen
 	
-}else{ if(userIncome < 750000)
+}else{ if(userIncome < 750000)// states if user income is less than 75000 then my alert should pop up
  	alert("You need to go to school for Training");}	
  	{ if(userIncome < 80000)
 	alert("Please be grateful for your job");
