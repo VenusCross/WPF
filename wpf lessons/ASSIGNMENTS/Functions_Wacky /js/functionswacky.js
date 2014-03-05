952e3990357bbd923ebc16356a60eefb56c677d1
venus <vcrossfullsail@gmail.com>
2014-03-04 22:52:21 -0500

    added correct way of adding space in an alert

--- wpf lessons/ASSIGNMENTS/Functions_Wacky /js/functionswacky.js
+++ wpf lessons/ASSIGNMENTS/Functions_Wacky /js/functionswacky.js
@@ -9,8 +9,8 @@ var creamConsumer = prompt("What is favorite ice creaam flavor strawberry, peach
 
 function iceCream(one,two){
 	alert("Pick a NEW Flavor!!!");
-	alert("These flavor taste delicious together" + "\n" + x);
+	alert("These flavor taste delicious together" "\n"+ one +"\n"+ two);// combos
 
 }
-iceCream("strawberry","chocolate");//
+iceCream("strawberry","chocolate");//testing code combos
 iceCream("peach","rainbow");
